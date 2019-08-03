cf234bbfbe8d2746556802aa93f9ecfe2bdcd5e6
joy <joy.lai@zg.com>
2019-08-03 12:09:38 +0800

    add

--- format_test.go
+++ format_test.go
@@ -1,9 +1,21 @@
 package go_format
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestFormatClass_Test(t *testing.T) {
-
+func TestFormatClass_StructToMap(t *testing.T) {
+	type Test struct {
+		A uint64 `json:"a"`
+		B string `json:"haha"`
+	}
+	test := Test{
+		A: 568262448,
+		B: `1111`,
+	}
+	fmt.Println(fmt.Sprintf(`%+v`, Format.StructToMap(test)))
+	if Format.StructToMap(test)[`a`].(float64) != 568262448 {
+		t.Error()
+	}
 }
