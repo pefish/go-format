b1e6d81b419f4312db3fc6f25f2d84598116b791
pefish <darrondarron@qq.com>
2023-09-07 11:49:44 +0800

    add

--- format.go
+++ format.go
@@ -671,7 +671,7 @@ func (ft *FormatType) GroupStringSlice(stringSlice []string, countPerGroup uint6
 
 		thisGroup := stringSlice[start:end]
 		resultGroup = append(resultGroup, thisGroup)
-		if len(thisGroup) < int(countPerGroup) {
+		if end-start < int(countPerGroup) {
 			break
 		}
 	}
